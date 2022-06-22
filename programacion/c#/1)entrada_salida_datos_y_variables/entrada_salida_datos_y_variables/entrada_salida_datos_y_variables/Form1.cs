c695afa685c0af43c3b00f9876ea33cce8a63fed
qu1r30n <qu1r30n@hotmail.com>
2022-06-22 15:32:11 -0500

    entrada_y_salidad_de_datos
    iniciando

---
+++ programacion/c#/1)entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entrada_salida_datos_y_variables
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ programacion/c#/1)entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entrada_salida_datos_y_variables
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
