e50b38d39eb54689eb3830cc42f9fb858def328c
qu1r30n <qu1r30n@hotmail.com>
2022-06-22 17:03:31 -0500

    operaciones basicas y tipos de variables

--- programacion/c#/1)entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/Form1.Designer.cs
+++ programacion/c#/1)entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/Form1.Designer.cs
@@ -29,13 +29,394 @@ namespace entrada_salida_datos_y_variables
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_sum_variable1 = new System.Windows.Forms.TextBox();
+            this.txt_sum_variable2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_resultado_sum = new System.Windows.Forms.Label();
+            this.btn_resul_suma = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_res_variable1 = new System.Windows.Forms.TextBox();
+            this.txt_res_variable2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lbl_resultado_res = new System.Windows.Forms.Label();
+            this.btn_resul_resta = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_mul_variable1 = new System.Windows.Forms.TextBox();
+            this.txt_mul_variable2 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbl_resultado_mult = new System.Windows.Forms.Label();
+            this.btn_resul_multiplicacion = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.txt_div_variable1 = new System.Windows.Forms.TextBox();
+            this.txt_div_variable2 = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.lbl_resultado_div = new System.Windows.Forms.Label();
+            this.btn_resul_divicion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "variable1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "variable2";
+            // 
+            // txt_sum_variable1
+            // 
+            this.txt_sum_variable1.Location = new System.Drawing.Point(15, 25);
+            this.txt_sum_variable1.Name = "txt_sum_variable1";
+            this.txt_sum_variable1.Size = new System.Drawing.Size(100, 20);
+            this.txt_sum_variable1.TabIndex = 2;
+            // 
+            // txt_sum_variable2
+            // 
+            this.txt_sum_variable2.Location = new System.Drawing.Point(178, 25);
+            this.txt_sum_variable2.Name = "txt_sum_variable2";
+            this.txt_sum_variable2.Size = new System.Drawing.Size(100, 20);
+            this.txt_sum_variable2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "+";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(284, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "=";
+            // 
+            // lbl_resultado_sum
+            // 
+            this.lbl_resultado_sum.AutoSize = true;
+            this.lbl_resultado_sum.Location = new System.Drawing.Point(303, 32);
+            this.lbl_resultado_sum.Name = "lbl_resultado_sum";
+            this.lbl_resultado_sum.Size = new System.Drawing.Size(50, 13);
+            this.lbl_resultado_sum.TabIndex = 6;
+            this.lbl_resultado_sum.Text = "resultado";
+            // 
+            // btn_resul_suma
+            // 
+            this.btn_resul_suma.Location = new System.Drawing.Point(359, 22);
+            this.btn_resul_suma.Name = "btn_resul_suma";
+            this.btn_resul_suma.Size = new System.Drawing.Size(75, 23);
+            this.btn_resul_suma.TabIndex = 7;
+            this.btn_resul_suma.Text = "procesar";
+            this.btn_resul_suma.UseVisualStyleBackColor = true;
+            this.btn_resul_suma.Click += new System.EventHandler(this.btn_resul_suma_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "variable1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(175, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "variable2";
+            // 
+            // txt_res_variable1
+            // 
+            this.txt_res_variable1.Location = new System.Drawing.Point(15, 84);
+            this.txt_res_variable1.Name = "txt_res_variable1";
+            this.txt_res_variable1.Size = new System.Drawing.Size(100, 20);
+            this.txt_res_variable1.TabIndex = 2;
+            // 
+            // txt_res_variable2
+            // 
+            this.txt_res_variable2.Location = new System.Drawing.Point(178, 84);
+            this.txt_res_variable2.Name = "txt_res_variable2";
+            this.txt_res_variable2.Size = new System.Drawing.Size(100, 20);
+            this.txt_res_variable2.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(137, 91);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(284, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "=";
+            // 
+            // lbl_resultado_res
+            // 
+            this.lbl_resultado_res.AutoSize = true;
+            this.lbl_resultado_res.Location = new System.Drawing.Point(303, 91);
+            this.lbl_resultado_res.Name = "lbl_resultado_res";
+            this.lbl_resultado_res.Size = new System.Drawing.Size(50, 13);
+            this.lbl_resultado_res.TabIndex = 6;
+            this.lbl_resultado_res.Text = "resultado";
+            // 
+            // btn_resul_resta
+            // 
+            this.btn_resul_resta.Location = new System.Drawing.Point(359, 81);
+            this.btn_resul_resta.Name = "btn_resul_resta";
+            this.btn_resul_resta.Size = new System.Drawing.Size(75, 23);
+            this.btn_resul_resta.TabIndex = 7;
+            this.btn_resul_resta.Text = "procesar";
+            this.btn_resul_resta.UseVisualStyleBackColor = true;
+            this.btn_resul_resta.Click += new System.EventHandler(this.btn_resul_resta_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(9, 131);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "variable1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(172, 131);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(50, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "variable2";
+            // 
+            // txt_mul_variable1
+            // 
+            this.txt_mul_variable1.Location = new System.Drawing.Point(12, 147);
+            this.txt_mul_variable1.Name = "txt_mul_variable1";
+            this.txt_mul_variable1.Size = new System.Drawing.Size(100, 20);
+            this.txt_mul_variable1.TabIndex = 2;
+            // 
+            // txt_mul_variable2
+            // 
+            this.txt_mul_variable2.Location = new System.Drawing.Point(175, 147);
+            this.txt_mul_variable2.Name = "txt_mul_variable2";
+            this.txt_mul_variable2.Size = new System.Drawing.Size(100, 20);
+            this.txt_mul_variable2.TabIndex = 3;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(134, 154);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(12, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "x";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(281, 154);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(13, 13);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "=";
+            // 
+            // lbl_resultado_mult
+            // 
+            this.lbl_resultado_mult.AutoSize = true;
+            this.lbl_resultado_mult.Location = new System.Drawing.Point(300, 154);
+            this.lbl_resultado_mult.Name = "lbl_resultado_mult";
+            this.lbl_resultado_mult.Size = new System.Drawing.Size(50, 13);
+            this.lbl_resultado_mult.TabIndex = 6;
+            this.lbl_resultado_mult.Text = "resultado";
+            // 
+            // btn_resul_multiplicacion
+            // 
+            this.btn_resul_multiplicacion.Location = new System.Drawing.Point(356, 144);
+            this.btn_resul_multiplicacion.Name = "btn_resul_multiplicacion";
+            this.btn_resul_multiplicacion.Size = new System.Drawing.Size(75, 23);
+            this.btn_resul_multiplicacion.TabIndex = 7;
+            this.btn_resul_multiplicacion.Text = "procesar";
+            this.btn_resul_multiplicacion.UseVisualStyleBackColor = true;
+            this.btn_resul_multiplicacion.Click += new System.EventHandler(this.btn_resul_multiplicacion_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(9, 203);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(50, 13);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "variable1";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(172, 203);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(50, 13);
+            this.label16.TabIndex = 1;
+            this.label16.Text = "variable2";
+            // 
+            // txt_div_variable1
+            // 
+            this.txt_div_variable1.Location = new System.Drawing.Point(12, 219);
+            this.txt_div_variable1.Name = "txt_div_variable1";
+            this.txt_div_variable1.Size = new System.Drawing.Size(100, 20);
+            this.txt_div_variable1.TabIndex = 2;
+            // 
+            // txt_div_variable2
+            // 
+            this.txt_div_variable2.Location = new System.Drawing.Point(175, 219);
+            this.txt_div_variable2.Name = "txt_div_variable2";
+            this.txt_div_variable2.Size = new System.Drawing.Size(100, 20);
+            this.txt_div_variable2.TabIndex = 3;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(134, 226);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(12, 13);
+            this.label17.TabIndex = 4;
+            this.label17.Text = "/";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(281, 226);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(13, 13);
+            this.label18.TabIndex = 5;
+            this.label18.Text = "=";
+            // 
+            // lbl_resultado_div
+            // 
+            this.lbl_resultado_div.AutoSize = true;
+            this.lbl_resultado_div.Location = new System.Drawing.Point(300, 226);
+            this.lbl_resultado_div.Name = "lbl_resultado_div";
+            this.lbl_resultado_div.Size = new System.Drawing.Size(50, 13);
+            this.lbl_resultado_div.TabIndex = 6;
+            this.lbl_resultado_div.Text = "resultado";
+            // 
+            // btn_resul_divicion
+            // 
+            this.btn_resul_divicion.Location = new System.Drawing.Point(356, 216);
+            this.btn_resul_divicion.Name = "btn_resul_divicion";
+            this.btn_resul_divicion.Size = new System.Drawing.Size(75, 23);
+            this.btn_resul_divicion.TabIndex = 7;
+            this.btn_resul_divicion.Text = "procesar";
+            this.btn_resul_divicion.UseVisualStyleBackColor = true;
+            this.btn_resul_divicion.Click += new System.EventHandler(this.btn_resul_divicion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_resul_divicion);
+            this.Controls.Add(this.btn_resul_multiplicacion);
+            this.Controls.Add(this.btn_resul_resta);
+            this.Controls.Add(this.btn_resul_suma);
+            this.Controls.Add(this.lbl_resultado_div);
+            this.Controls.Add(this.lbl_resultado_mult);
+            this.Controls.Add(this.lbl_resultado_res);
+            this.Controls.Add(this.lbl_resultado_sum);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_div_variable2);
+            this.Controls.Add(this.txt_mul_variable2);
+            this.Controls.Add(this.txt_res_variable2);
+            this.Controls.Add(this.txt_sum_variable2);
+            this.Controls.Add(this.txt_div_variable1);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.txt_mul_variable1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txt_res_variable1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txt_sum_variable1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_sum_variable1;
+        private System.Windows.Forms.TextBox txt_sum_variable2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_resultado_sum;
+        private System.Windows.Forms.Button btn_resul_suma;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_res_variable1;
+        private System.Windows.Forms.TextBox txt_res_variable2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_resultado_res;
+        private System.Windows.Forms.Button btn_resul_resta;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_mul_variable1;
+        private System.Windows.Forms.TextBox txt_mul_variable2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lbl_resultado_mult;
+        private System.Windows.Forms.Button btn_resul_multiplicacion;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox txt_div_variable1;
+        private System.Windows.Forms.TextBox txt_div_variable2;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label lbl_resultado_div;
+        private System.Windows.Forms.Button btn_resul_divicion;
     }
 }
 
--- programacion/c#/1)entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/Form1.cs
+++ programacion/c#/1)entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/entrada_salida_datos_y_variables/Form1.cs
@@ -12,9 +12,58 @@ namespace entrada_salida_datos_y_variables
 {
     public partial class Form1 : Form
     {
+        int resultado_sum;
+        double resultado_resta_doble;
+        float resultado_multiplicacion_float;
+        string resultado_divicion_cadena_de_texto;
+        char caracter;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_resul_suma_Click(object sender, EventArgs e)
+        {
+            string var1_string;
+            string var2_string;
+            var1_string = txt_sum_variable1.Text;
+            var2_string = txt_sum_variable2.Text;
+
+            int var1_entero = Convert.ToInt32(var1_string);
+            int var2_entero = Convert.ToInt32(var2_string);
+            resultado_sum = var1_entero + var2_entero;
+
+            lbl_resultado_sum.Text = ""+resultado_sum;
+
+        }
+
+        private void btn_resul_resta_Click(object sender, EventArgs e)
+        {
+            double var1_double = Convert.ToDouble(txt_res_variable1.Text);
+            double var2_double = Convert.ToDouble(txt_res_variable2.Text);
+
+            resultado_resta_doble = var1_double - var2_double;
+            lbl_resultado_res.Text = "" + resultado_resta_doble;
+            
+        }
+
+        private void btn_resul_multiplicacion_Click(object sender, EventArgs e)
+        {
+            decimal var1_float = Convert.ToDecimal(txt_mul_variable1.Text);
+            decimal var2_float = Convert.ToDecimal(txt_mul_variable2.Text);
+
+            resultado_multiplicacion_float = Convert.ToSingle(var1_float * var2_float);
+            lbl_resultado_res.Text = "" + resultado_multiplicacion_float;
+
+        }
+
+        private void btn_resul_divicion_Click(object sender, EventArgs e)
+        {
+            string var1_string = txt_div_variable1.Text;
+            string var2_string = txt_div_variable2.Text;
+
+            resultado_divicion_cadena_de_texto = (Convert.ToDecimal(var1_string) / Convert.ToDecimal(var2_string)).ToString();
+            lbl_resultado_div.Text = resultado_divicion_cadena_de_texto;
         }
     }
 }
